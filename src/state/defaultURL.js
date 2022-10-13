e919e74664e8fbf29e781827e153f6462af6218c
Yada Martins Tisan <tisanyada@gmail.com>
2022-10-13 10:10:08 +0100

    Updated Urls

--- src/state/defaultURL.js
+++ src/state/defaultURL.js
@@ -9,9 +9,12 @@ if (process.env.NODE_ENV === 'development') {
     departmentUrl = "http://localhost:3001/department"
 } else {
     // production
-    studentUrl = "https://algopay-server.herokuapp.com/students"
-    facultyUrl = "https://algopay-server.herokuapp.com/faculty"
-    departmentUrl = "https://algopay-server.herokuapp.com/department"
+    // studentUrl = "https://algopay-server.herokuapp.com/students"
+    // facultyUrl = "https://algopay-server.herokuapp.com/faculty"
+    // departmentUrl = "https://algopay-server.herokuapp.com/department"
+    studentUrl1="https://web-production-6f07.up.railway.app/students"
+    facultyUrl="https://web-production-6f07.up.railway.app/faculty"
+    departmentUrl="https://web-production-6f07.up.railway.app/department"
 }
 
 
